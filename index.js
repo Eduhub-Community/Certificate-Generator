27d84faa9daa8a6f17b0e4e19e00a94cf53ff7c8
Sahitya Roy <72821604+SahityaRoy@users.noreply.github.com>
2021-08-30 21:35:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ submit.addEventListener('click',()=>{
 const generetPdf = async (name,cr)=>{
     const {PDFDocument,rgb} = PDFLib;
 
-    const exBytes = await fetch("./twitter.pdf").then((res)=>{
+    const exBytes = await fetch("./outreachy.pdf").then((res)=>{
         return res.arrayBuffer()
     });
 
